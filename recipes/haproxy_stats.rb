1a3c8887090eb746b5cf68ecd0fe70566c11a859
Muerdter, Nick <nick.muerdter@nrel.gov>
2012-05-24 04:31:28 +0000

    I think this node searching is now working with Chef 0.10.10.

--- recipes/haproxy_stats.rb
+++ recipes/haproxy_stats.rb
@@ -7,16 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# FIXME: Chef attribute searching seems to be broken? Fall back to searching
-# for all servers and manually looking for the monitoring role for now.
-#
-# monitoring_servers = search(:node, 'roles:monitoring')
-monitoring_servers = []
-search(:node, '*:*').each do |node|
-  if node.roles.include?("monitoring")
-    monitoring_servers << node
-  end
-end
+monitoring_servers = search(:node, 'roles:monitoring')
 
 iptables_rule "haproxy_stats" do
   variables :monitoring_servers => monitoring_servers
